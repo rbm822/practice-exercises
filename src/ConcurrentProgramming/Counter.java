f7422b32ccf859ee18f14f283e02292f7ff2437c
rbm822 <54344863+rbm822@users.noreply.github.com>
2022-02-17 04:30:16 +0900

    Followed a Baeldung tutorial about synchronized methods.

---
+++ src/ConcurrentProgramming/Counter.java
@@ -0,0 +1,17 @@
+package ConcurrentProgramming;
+
+public class Counter {
+    private int c = 0;
+
+    public void increment() {
+        c++;
+    }
+
+    public void decrement() {
+        c--;
+    }
+
+    public int value() {
+        return c;
+    }
+}
